8f571c9ea2f00df0c8009f6ba2316e21aef454d5
Freem11 <freem1985@gmail.com>
2021-10-29 18:11:53 -0700

    mode swithcing hook ready

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+export default function useVisualMode(initial) {
+
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); 
+
+
+  function transition(nextMode, replace){
+    if (replace === true){
+    setMode(nextMode)  
+    } else {
+    setMode(nextMode)   
+    setHistory([...history, nextMode])
+    }
+  }
+
+  function back(){
+    if(history.length > 1) {
+      setHistory(history.slice(0,-1))
+      setMode(history.slice(-2)[0])
+    }
+  } 
+
+  console.log("history", history)
+  console.log("mode", mode)
+  return { mode, transition, back};
+
+
+  
+
+};
